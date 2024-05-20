4a31f3ff988770963885e38070a0ce77353d2ba6
Pavan Katta <pavanyellow@gmail.com>
2024-05-20 13:33:45 +0530

    a

---
+++ a.py
@@ -0,0 +1,11 @@
+import requests
+
+resp = requests.post(
+    "https://model-e3mv6pz3.api.baseten.co/production/predict",
+    headers={"Authorization": "Api-Key Fn7R3A6B.sNl9j0G1rHVCZSjrHHcT6ZAnHsF2T2n8"},
+    json={'top_p': 0.75, 'prompt': "What's the meaning of life?", 'num_beams': 4, 'temperature': 0.1},
+    stream=True
+)
+
+for content in resp.iter_content():
+    print(content.decode("utf-8"), end="", flush=True)
